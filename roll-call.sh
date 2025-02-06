0e0b16a4be823f7d8aa42b9c3d178062753798a6
BwithE <144924113+BwithE@users.noreply.github.com>
2025-02-06 00:03:42 -0500

    Update roll-call.sh

--- roll-call.sh
+++ roll-call.sh
@@ -30,19 +30,22 @@ if [ -z "$ip" ]; then
     usage
 fi
 
-# creates dashboard report
-exec > >(tee $ip\_report.txt) 2>&1
+# Create main directory
+mkdir -p $ip
+
+# Create dashboard report
+exec > >(tee $ip/REPORT.txt) 2>&1
 
 function nmap_query {
+    mkdir -p $ip/port_scan
     echo -e "[+] Starting nmap port scan:"
-    echo "COMMAND: nmap -p- --open $ip" > nmap_ports.txt
-    echo "" >> nmap_ports.txt
-    echo "" >> nmap_ports.txt
-    nmap -p- --open $ip >> nmap_ports.txt
+    echo "COMMAND: nmap -p- --open $ip" > $ip/port_scan/nmap_ports.txt
+    echo -e "\n\n\n" >> $ip/port_scan/nmap_ports.txt
+    nmap -p- --open $ip >> $ip/port_scan/nmap_ports.txt
 }
 
 function open_ports {
-    ports=$(cat nmap_ports.txt | egrep open | egrep "tcp|udp" | egrep -o "^[0-9]++++")
+    ports=$(cat $ip/port_scan/nmap_ports.txt | egrep open | egrep "tcp|udp" | egrep -o "^[0-9]+")
     for i in $ports
     do
         echo -e "${ORANGE}[info] Open Port: $i${NC}"
@@ -53,81 +56,71 @@ function nmap_service {
     echo -e "[+] Starting nmap service scan:"
     for i in $ports
     do
-        echo "COMMAND: nmap -A -p $i $ip" > nmap_port_$i\_service.txt
-	echo "" >> nmap_port_$i\_service.txt
-        echo "" >> nmap_port_$i\_service.txt
-	nmap -A -p $i $ip >> nmap_port_$i\_service.txt
+        mkdir -p $ip/${i}_info
+        echo "COMMAND: nmap -A -p $i $ip" > $ip/${i}_info/nmap_port_${i}_service.txt
+        echo -e "\n\n\n" >> $ip/${i}_info/nmap_port_${i}_service.txt
+        nmap -A -p $i $ip >> $ip/${i}_info/nmap_port_${i}_service.txt
+        services=$(cat $ip/${i}_info/nmap_port_${i}_service.txt | egrep open | egrep "^$i" )
+        echo -e "${ORANGE}[info] $services${NC}"
+        sir=$(cat $ip/${i}_info/nmap_port_${i}_service.txt | egrep "^[0-9]+++++"| egrep "$i" | awk '{print $3}')
+        mv $ip/${i}_info $ip/${i}_$sir\_info
     done
 }
 
-function service_enumeration {
-    for i in $ports
-    do
-        services=$(cat nmap_port_$i\_service.txt | egrep open | egrep "^$i" )
-        echo -e "${ORANGE}[info] $services${NC}"
-    done
-}
 
 function ftp_enumeration {
     for i in $ports
     do
-        ftp_query=$(cat nmap_port_$i\_service.txt | egrep open | egrep "^$i" | egrep -wo ftp)
-        ftp_port=$(cat nmap_port_$i\_service.txt | egrep open | egrep "^$i" | egrep -w ftp | cut -d '/' -f 1)
+        ftp_query=$(cat $ip/${i}_ftp_info/nmap_port_${i}_service.txt 2>/dev/null | egrep open | egrep "^$i" | egrep -wo ftp)
+        ftp_port=$(cat $ip/${i}_ftp_info/nmap_port_${i}_service.txt 2>/dev/null | egrep open | egrep "^$i" | egrep -w ftp | cut -d '/' -f 1)
         if [ -z "$ftp_port" ]
         then
-	    continue
+            continue
         else
-            echo -e "[+] Starting Hydra FTP enumeration on Port: $ftp_port" 
-	    echo "COMMAND: hydra -C /usr/share/wordlists/seclists/Passwords/Default-Credentials/ftp-betterdefaultpasslist.txt ftp://$ip -V -s $ftp_port" > ftp_port_$ftp_port\_enumeration.txt
-     	    echo "" >> ftp_port_$ftp_port\_enumeration.txt
-	    echo "" >> ftp_port_$ftp_port\_enumeration.txt
-            hydra -C /usr/share/wordlists/seclists/Passwords/Default-Credentials/ftp-betterdefaultpasslist.txt ftp://$ip -V -s $ftp_port >> ftp_port_$ftp_port\_enumeration.txt
-	    sleep 1
-            results=$(egrep "^\[$ftp_port]" ftp_port_$ftp_port\_enumeration.txt)
+            echo -e "[+] Starting Hydra FTP enumeration on Port: $ftp_port"
+            echo "COMMAND: hydra -C /usr/share/wordlists/seclists/Passwords/Default-Credentials/ftp-betterdefaultpasslist.txt ftp://$ip -V -s $ftp_port" > $ip/${i}_ftp_info/hydra_ftp_${ftp_port}.txt
+            echo -e "\n\n\n" >> $ip/${i}_ftp_info/hydra_ftp_${ftp_port}.txt
+            hydra -C /usr/share/wordlists/seclists/Passwords/Default-Credentials/ftp-betterdefaultpasslist.txt ftp://$ip -V -s $ftp_port >> $ip/${i}_ftp_info/hydra_ftp_${ftp_port}.txt
+            sleep 1
+            results=$(egrep "^\[$ftp_port]" $ip/${i}_ftp_info/hydra_ftp_${ftp_port}.txt)
             echo -e "${GREEN}[NUGGET] $results${NC}"
         fi
     done
 }
 
-
 function ssh_enumeration {
     for i in $ports
     do
-        ssh_query=$(cat nmap_port_$i\_service.txt | egrep open | egrep "^$i" | egrep -wo ssh)
-        ssh_port=$(cat nmap_port_$i\_service.txt | egrep open | egrep "^$i" | egrep -w ssh | cut -d '/' -f 1)
+        ssh_query=$(cat $ip/${i}_ssh_info/nmap_port_${i}_service.txt 2>/dev/null | egrep open | egrep "^$i" | egrep -wo ssh)
+        ssh_port=$(cat $ip/${i}_ssh_info/nmap_port_${i}_service.txt 2>/dev/null | egrep open | egrep "^$i" | egrep -w ssh | cut -d '/' -f 1)
         if [ -z "$ssh_port" ]
         then
             continue
         else
-            echo -e "[+] Starting Hydra SSH enumeration on Port: $ssh_port" 
-            echo "COMMAND: hydra -C /usr/share/wordlists/seclists/Passwords/Default-Credentials/ssh-betterdefaultpasslist.txt ssh://$ip -V -s $ssh_port -t 1 2>/dev/null" > ssh_port_$ssh_port\_enumeration.txt
-	    echo "" >> ssh_port_$ssh_port\_enumeration.txt
-     	    echo "" >> ssh_port_$ssh_port\_enumeration.txt
-     	    hydra -C /usr/share/wordlists/seclists/Passwords/Default-Credentials/ssh-betterdefaultpasslist.txt ssh://$ip -V -s $ssh_port -t 1 2>/dev/null >> ssh_port_$ssh_port\_enumeration.txt
+            echo -e "[+] Starting Hydra SSH enumeration on Port: $ssh_port"
+            echo "COMMAND: hydra -C /usr/share/wordlists/seclists/Passwords/Default-Credentials/ssh-betterdefaultpasslist.txt ssh://$ip -V -s $ssh_port -t 1 2>/dev/null" > $ip/${i}_ssh_info/hydra_ssh_${ssh_port}.txt
+            echo -e "\n\n\n" >> $ip/${i}_ssh_info/hydra_ssh_${ssh_port}.txt
+            hydra -C /usr/share/wordlists/seclists/Passwords/Default-Credentials/ssh-betterdefaultpasslist.txt ssh://$ip -V -s $ssh_port -t 1 2>/dev/null >> $ip/${i}_ssh_info/hydra_ssh_${ssh_port}.txt
             sleep 1
-            results=$(egrep "^\[$ssh_port]" ssh_port_$ssh_port\_enumeration.txt)
-            if $(cat ssh_port_$ssh_port\_enumeration.txt | grep -q "could not be completed")
-	    then
+            results=$(egrep "^\[$ssh_port]" $ip/${i}_ssh_info/hydra_ssh_${ssh_port}.txt)
+            if $(cat $ip/${i}_ssh_info/hydra_ssh_${ssh_port}.txt | grep -q "could not be completed")
+            then
                 echo -e "${RED}[error] COULDN'T FINISH Hydra SSH enumeration: SUGGEST MANUAL ENUMERATION${NC}"
-            elif $(cat ssh_port_$ssh_port\_enumeration.txt | grep -q "0 valid password found")
-	    then
-                 echo -e "${ORANGE}[info] 0 PASSWORDS FOUND${NC}"
-	    else
+            elif $(cat $ip/${i}_ssh_info/hydra_ssh_${ssh_port}.txt | grep -q "0 valid password found")
+            then
+                echo -e "${ORANGE}[info] 0 PASSWORDS FOUND${NC}"
+            else
                 echo -e "${GREEN}[NUGGET] $results${NC}"
             fi
         fi
     done
 }
 
-
-
 nmap_query
 
 open_ports
 
 nmap_service
-
-service_enumeration
 
 ftp_enumeration
 
