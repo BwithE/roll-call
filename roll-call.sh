bdc0bef9159f028cd629a0c77b168b6e4fdeef7d
BwithE <144924113+BwithE@users.noreply.github.com>
2025-02-18 07:11:49 -0500

    Update roll-call.sh

--- roll-call.sh
+++ roll-call.sh
@@ -39,20 +39,20 @@ exec > >(tee $ip/REPORT.txt) 2>&1
 
 # initial nmap scan
   mkdir -p $ip/port_scan
-  echo -e "${BLUE}[+] Starting nmap port scan:${NC}"
-  echo "COMMAND: nmap -p- --open $ip" > $ip/port_scan/nmap_ports.txt
-  echo -e "\n\n\n" >> $ip/port_scan/nmap_ports.txt
-  nmap -p- -Pn --open $ip >> $ip/port_scan/nmap_ports.txt
+  echo -e "${ORANGE}[+] Starting nmap port/service scan:${NC}"
+  echo "COMMAND: nmap -p- -sV -Pn --open $ip" > $ip/port_scan/nmap_scan.txt
+  echo -e "\n\n\n" >> $ip/port_scan/nmap_scan.txt
+  nmap -p- -sV -Pn --open $ip >> $ip/port_scan/nmap_scan.txt
 
 # open ports
-  ports=$(cat $ip/port_scan/nmap_ports.txt | egrep open | egrep "tcp|udp" | egrep -o "^[0-9]+")
+  ports=$(cat $ip/port_scan/nmap_scan.txt | egrep open | egrep "tcp|udp" | egrep -o "^[0-9]+")
   for i in $ports
   do
       echo -e "${GREEN}[info] Open Port: $i${NC}"
   done
 
 
-
+nmap_question () {
 # Prompt the user to choose between scanning all ports or selecting specific ports
 echo -e "${YELLOW}[input] Would you like to scan all ports or select specific ones?${NC}"
 echo -e "${ORANGE}   1) Aggressive Scan all open ports individually (SLOWER)${NC}"
@@ -61,7 +61,7 @@ read -p "Please select an option [1/2]: " choice
 # If the user selects option 1, proceed with scanning all ports
 if [ "$choice" -eq 1 ]
 then
-     ports=$(cat $ip/port_scan/nmap_ports.txt | egrep open | egrep "tcp|udp" | egrep -o "^[0-9]+")
+     ports=$(cat $ip/port_scan/nmap_scan.txt | egrep open | egrep "tcp|udp" | egrep -o "^[0-9]+")
 # If the user selects option 2, ask for specific ports and proceed
 elif [ "$choice" -eq 2 ]; then
     read -p "Enter the ports to scan, separated by spaces (EXAMPLE: 21 22 80 445): " selected_ports
@@ -70,22 +70,24 @@ else
     echo -e "${RED}[error] Invalid choice, exiting.${NC}"
     exit 1
 fi
+}
 
+#nmap_question
+
+
+ports=$(cat $ip/port_scan/nmap_scan.txt | egrep open | egrep "tcp|udp" | egrep -o "^[0-9]+")
 #####################
 # nmap service scan #
 #####################
-echo -e "${BLUE}[+] Starting nmap service scan:${NC}"
+echo -e "${ORANGE}[+] Starting service enumeration:${NC}"
 for i in $ports
 do
     sleep 1
     # create directories on ports
     mkdir -p $ip/${i}_info
-    echo "COMMAND: nmap -A -p $i $ip" > $ip/${i}_info/nmap_port_${i}_service.txt
-    echo -e "\n\n\n" >> $ip/${i}_info/nmap_port_${i}_service.txt
-    nmap -sV -p $i $ip >> $ip/${i}_info/nmap_port_${i}_service.txt
-    sirs=$(cat $ip/${i}_info/nmap_port_${i}_service.txt | egrep open | egrep "^$i" )
+    sirs=$(cat $ip/port_scan/nmap_scan.txt | egrep open | egrep "tcp|udp" | egrep "^$i" )
     echo -e "${GREEN}[info] $sirs${NC}"
-    services=$(cat $ip/${i}_info/nmap_port_${i}_service.txt | egrep "^[0-9]+++++"| egrep "$i" | awk '{print $3}')
+    services=$(cat $ip/port_scan/nmap_scan.txt | egrep "^[0-9]+++++"| egrep "$i" | awk '{print $3}')
     sleep 1
 
 
@@ -100,7 +102,7 @@ do
             http|https|http?)
                 sleep 1
                 # gobuster for directories
-                echo -e "${BLUE}[+] Starting http enumeration with gobuster/dirb/nikto on Port: $i${NC}"
+                echo -e "${BLUE}[+] Starting http enumeration with gobuster/dirb/nikto/whatweb on Port: $i${NC}"
                 echo "COMMAND: gobuster dir -u http://$ip:$i -w /usr/share/wordlists/seclists/Discovery/Web-Content/raft-large-directories.txt -q" > $ip/${i}_http_info/gobuster_directories.txt
                 echo -e "\n\n\n" >> $ip/${i}_http_info/gobuster_directories.txt
                 gobuster dir -u http://$ip:$i -w /usr/share/wordlists/seclists/Discovery/Web-Content/raft-large-directories.txt -q >> $ip/${i}_http_info/gobuster_directories.txt & 
@@ -117,17 +119,17 @@ do
                 sleep 1
 
                 # dirb for directories
-                echo "COMMAND: dirb http://$ip:$i -l /usr/share/wordlists/seclists/Discovery/Web-Content/raft-large-directories.txt" > $ip/${i}_http_info/dirb_directories.txt
+                echo "COMMAND: dirb http://$ip:$i /usr/share/wordlists/seclists/Discovery/Web-Content/raft-large-directories.txt" > $ip/${i}_http_info/dirb_directories.txt
                 echo -e "\n\n\n" >> $ip/${i}_http_info/dirb_directories.txt
-                dirbdir=$(dirb http://$ip:$i -l /usr/share/wordlists/seclists/Discovery/Web-Content/raft-large-directories.txt >> $ip/${i}_http_info/dirb_directories.txt &)
+                dirbdir=$(dirb http://$ip:$i /usr/share/wordlists/seclists/Discovery/Web-Content/raft-large-directories.txt >> $ip/${i}_http_info/dirb_directories.txt &)
                 #dirbydir_pid=$!
                 #echo -e "${GREEN}[info] Please review: $ip/${i}_http_info/dirb_directories.txt${NC}"
                 sleep 1
 
                 # dirb for files
-                echo "COMMAND: dirb http://$ip:$i -l /usr/share/wordlists/seclists/Discovery/Web-Content/raft-large-files.txt" > $ip/${i}_http_info/dirb_files.txt
+                echo "COMMAND: dirb http://$ip:$i /usr/share/wordlists/seclists/Discovery/Web-Content/raft-large-files.txt" > $ip/${i}_http_info/dirb_files.txt
                 echo -e "\n\n\n" >> $ip/${i}_http_info/dirb_files.txt
-                dirbfile=$(dirb http://$ip:$i -l /usr/share/wordlists/seclists/Discovery/Web-Content/raft-large-files.txt >> $ip/${i}_http_info/dirb_files.txt &)
+                dirbfile=$(dirb http://$ip:$i /usr/share/wordlists/seclists/Discovery/Web-Content/raft-large-files.txt >> $ip/${i}_http_info/dirb_files.txt &)
                 #dirbyfile_pid=$!
                 #echo -e "${GREEN}[info] Please review: $ip/${i}_http_info/dirb_files.txt${NC}"
                 sleep 1
@@ -136,6 +138,14 @@ do
                 echo "COMMAND: nikto -h http://$ip:$i -Tuning 5" > $ip/${i}_http_info/nikto_scan.txt
                 echo -e "\n\n\n" >> $ip/${i}_http_info/nikto_scan.txt
                 nicky=$(nikto -h http://$ip:$i -Tuning 5 >> $ip/${i}_http_info/nikto_scan.txt 2>/dev/null &)
+                #niktor_pid=$!
+                #echo -e "${GREEN}[info] Please review: $ip/${i}_http_info/nikto_scan.txt${NC}"
+                sleep 1
+
+                # whatweb for vulnerability scan
+                echo "COMMAND: whatweb http://$ip:$i " > $ip/${i}_http_info/whatweb_scan.txt
+                echo -e "\n\n\n" >> $ip/${i}_http_info/whatweb_scan.txt
+                whatwebber=$(whatweb http://$ip:$i >> $ip/${i}_http_info/whatweb_scan.txt 2>/dev/null &)
                 #niktor_pid=$!
                 #echo -e "${GREEN}[info] Please review: $ip/${i}_http_info/nikto_scan.txt${NC}"
                 sleep 1
@@ -171,9 +181,9 @@ do
                 enum4linux $ip 2>/dev/null >> $ip/${i}_$service\_info/enum4linux_results_${i}.txt &
                 sleep 1
 
-                echo "COMMAND: smbclient \\\\$ip -U '' -P '' " >  $ip/${i}_$service\_info/smbclient_results_${i}.txt
+                echo "COMMAND: smbclient -N -L \\\\$ip " >  $ip/${i}_$service\_info/smbclient_results_${i}.txt
                 echo -e "\n\n\n" >> $ip/${i}_$service\_info/smbclient_results_${i}.txt
-                smbclient \\\\$ip -U '' -P '' >> $ip/${i}_$service\_info/smbclient_results_${i}.txt 2>&1 &
+                smbclient -N -L \\\\$ip >> $ip/${i}_$service\_info/smbclient_results_${i}.txt 2>&1 &
                 sleep 1
 
                 # handle the results and display appropriate messages (OLD REPORTING, keeping for later use)
@@ -191,6 +201,7 @@ do
                 '
                 ;;
             ssh|ssh?)
+                function_ssh () {
                 sleep 1
                 echo -e "${BLUE}[+] Starting Hydra SSH enumeration on Port: $i${NC}"
                 echo "COMMAND: hydra -C /usr/share/wordlists/seclists/Passwords/Default-Credentials/ssh-betterdefaultpasslist.txt ssh://$ip -V -s $i -t 1 2>/dev/null" > $ip/${i}_ssh_info/hydra_ssh_${i}.txt
@@ -211,6 +222,7 @@ do
                     echo -e "${GREEN}[info] 0 SSH PASSWORDS FOUND${NC}"
                 fi
                 '
+                }
                 ;;
             dns|dns?)
                 # Add any DNS related enumeration here
@@ -229,6 +241,14 @@ do
                 echo "COMMAND: nbtscan $ip" > $ip/${i}_$service\_info/nbtscan_results.txt
                 echo -e "\n\n\n" >> $ip/${i}_$service\_info/nbtscan_results.txt
                 netbios_test=$(nbtscan $ip >> $ip/${i}_$service\_info/nbtscan_results.txt &)
+                sleep 1
+                ;;
+            smtp|smtp?|mail)
+                sleep 1
+                echo -e "${BLUE}[+] Starting SMTP enumeration on Port: $i${NC}"
+                echo "COMMAND: nmap -p ${i} --script=smtp-enum-users $ip" > $ip/${i}_$service\_info/smtp_nmap_enumusers_script.txt
+                echo -e "\n\n\n" >> $ip/${i}_$service\_info/smtp_nmap_enumusers_script.txt
+                smtp_enumusers=$(nmap -p ${i} --script=smtp-enum-users $ip >> $ip/${i}_$service\_info/smtp_nmap_enumusers_script.txt &)
                 sleep 1
                 ;;
         esac
@@ -250,23 +270,27 @@ count_active_processes() {
     gobuster_count=$(ps aux | egrep -w 'gobuster' | egrep -v "color=auto" | egrep -v "grep" | wc -l)
     dirb_count=$(ps aux | egrep -w 'dirb' | egrep -v "color=auto" | egrep -v "grep" | wc -l)
     nikto_count=$(ps aux | egrep -w 'nikto' | egrep -v "color=auto" | egrep -v "grep" | wc -l)
+    whatweb_count=$(ps aux | grep -w 'whatweb' | egrep -v 'color=auto' | egrep -v 'grep' | wc -l)
     enum4linux_count=$(ps aux | egrep -w 'enum4linux' | egrep -v "color=auto" | egrep -v "grep" | wc -l)
     rpc_count=$(ps aux | egrep -w 'rcpclient'| egrep -v "color=auto" | egrep -v "grep" | wc -l)
     netbios_count=$(ps aux | egrep -w 'nbtscan'| egrep -v "color=auto" | egrep -v "grep" | wc -l)
     smbclient_count=$(ps aux | egrep -w 'smbclient '| egrep -v "color=auto" | egrep -v "grep" | wc -l)
+    smtp_enumusers_count=$(ps aux | egrep -w 'nmap -p 25 --script=smtp-enum-users' | egrep -v "color=auto" | egrep -v "grep" | wc -l )
 
-    total_processes_count=$(($ftphydra_count + $sshhydra_count + $gobuster_count + $dirb_count + $nikto_count + $enum4linux_count + $rpc_count + $netbios_count + $smbclient_count))
+    total_processes_count=$(($smtp_enumusers_count + $whatweb_count + $ftphydra_count + $sshhydra_count + $gobuster_count + $dirb_count + $nikto_count + $enum4linux_count + $rpc_count + $netbios_count + $smbclient_count))
     echo $total_processes_count
 }
 
 # Check for user input while background tasks are running
-echo -e "${BLUE}[+] Waiting on the background scans to finish. Press the ENTER/RETURN key to check progress.${NC}"
-
+echo -e "${ORANGE}[+] Waiting on the background scans to finish. Press the ENTER/RETURN key to check progress.${NC}"
+active_processes=$(count_active_processes)
+echo -e "${PINK}[endgame] Active Enumeration Processes: $active_processes${NC}"
 # Loop to monitor processes
 while true; do
     # Wait for a keypress without blocking other tasks
     if read -t 1 -n 1; then
         # If a key was pressed, count active processes and display
+        echo -e "${ORANGE}[+] Waiting on the background scans to finish. Press the ENTER/RETURN key to check progress.${NC}"
         active_processes=$(count_active_processes)
         echo -e "${PINK}[endgame] Active Enumeration Processes: $active_processes${NC}"
     fi
